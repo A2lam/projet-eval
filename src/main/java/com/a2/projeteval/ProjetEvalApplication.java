0ed013b9528a2671d7a4adb5b4a69804ac1827c3
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-07-16 16:20:56 +0200

    New maven project

---
+++ src/main/java/com/a2/projeteval/ProjetEvalApplication.java
@@ -0,0 +1,13 @@
+package com.a2.projeteval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetEvalApplication.class, args);
+	}
+
+}
